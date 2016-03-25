0526075cda5ddf8e8cba9eab1431595ca6d4deaf
crodeheaver <colbyrodeheaver@hotmail.com>
2016-03-25 08:59:04 -0500

    update to creating a new student

--- app/students/new/route.js
+++ app/students/new/route.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return this.store.createRecord('student');
+  },
+  actions: {
+    addStudent(newStudent) {
+      console.log(newStudent.get('first-name'));
+      newStudent.save().then(() => this.transitionTo('students.index'));
+    },
+
+    willTransition() {
+      // rollbackAttributes() removes the record from the store
+      // if the model 'isNew'
+      this.controller.get('model').rollbackAttributes();
+    }
+  }
 });
